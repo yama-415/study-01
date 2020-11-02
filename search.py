5b5dd2b7d61ed63747ae957c9fbd3578f54b7d3f
yamako <73749043+yama-415@users.noreply.github.com>
2020-11-02 19:37:41 +0900

    Add files via upload

--- search.py
+++ search.py
@@ -4,45 +4,36 @@
 
 import csv
 # 検索ソース
-with open('source.csv', 'w') as csv_file:
-    writer = csv.writer(csv_file)
-    writer.writerow(["ねずこ","たんじろう","きょうじゅろう","ぎゆう","げんや","かなお","ぜんいつ"])
+
+source = ['ねずこ','たんじろう','きょうじゅろう','ぎゆう','げんや','かなお','ぜんいつ']
 
 ### 検索ツール
-
-with open('source.csv', 'r') as csv_file:
-    reader = csv.reader(csv_file)
-    for row in reader:
-        print(row)
-
-# print(source)
-
+        
 def search():
     word =input("鬼滅の登場人物の名前を入力してください >>> ")
     
     ### ここに検索ロジックを書く
-    print(word in row)
+    print(word in source)
 
-    if word in row:
+    if word in source:
         print('います')
     else:
-        row.append(word)
+        source.append(word)
         print('いません。追加します。')
-
-    print(row)
 
     print("{}が見つかりした".format(word))
 
 if __name__ == "__main__":
     search()
 
-# 現時点で試行錯誤中です↓
-# with open('source.csv', 'w') as csv_file:
-#     writer = csv.writer(csv_file)
-#     writer.writerow(word)
+
+with open('source.csv', 'w') as csv_file:
+    writer = csv.writer(csv_file)
+    writer.writerow(source)
+
+with open('source.csv', 'r') as csv_file:
+    reader = csv.reader(csv_file)
+    for row in reader:
+        print(row)
     
-
-
-
-
 
